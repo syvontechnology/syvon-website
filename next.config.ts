82275a7a43003919b7f092bff3e806e8b59ffd0f
Swami @ iSpark <swamik@isparklearning.com>
2026-01-27 20:23:42 +0530

    favicon

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactCompiler: true,
+
+  // REQUIRED for static hosting (GCS)
+  output: "export",
+
+  // Optional but recommended
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
